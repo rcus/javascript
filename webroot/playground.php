70fb2a2c6ec89fdf02c017fc8b87aec12774ed98
Marcus Törnroth <m@rcus.se>
2015-09-09 11:28:37 +0200

    Playground style updated

--- webroot/playground.php
+++ webroot/playground.php
@@ -15,13 +15,21 @@ $herbert['main'] = <<<EOD
 <h1>Lekplats för JavaScript</h1>
 <div class="boxed">
   <p><em>Här kommer lite blandat material upp som man kan leka med.</em></p>
-  <h2 id="mall"><a href="lekplats/mall/">Mall</a></h2>
+
+  <h2 id="mall">Mall
+  <ul>
+    <li><a href="lekplats/mall/">Lek nu!</a></li>
+    <li><a href="source.php?path=mall/index.php">Visa källkoden</a></li>
+    <li><a href="http://jsfiddle.net/rcus/60mj43hv/">JSFiddle</a></li>
+  </ul></h2>
   <p>Första skriptet. Mest gjort för att skapa en struktur för fortsatt lek ;)</p>
+
 </div>
 <div class="sidebar">
   <h3>Användbart:</h3>
   <ul>
     <li><a href="source.php">Visa källkod</a></li>
+    <li><a href="http://jsfiddle.net/user/rcus/">JSFiddle</a></li>
   </ul>
 </div>
 EOD;
