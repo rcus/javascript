1635c88555c25ba7d6e745104bd10194a074e870
Marcus Törnroth <m@rcus.se>
2015-09-11 00:40:32 +0200

    kmom01 ready

--- webroot/playground.php
+++ webroot/playground.php
@@ -16,13 +16,44 @@ $herbert['main'] = <<<EOD
 <div class="boxed">
   <p><em>Här kommer lite blandat material upp som man kan leka med.</em></p>
 
+  <h2 id="transforms-and-transitions">Transforms och transitions
+    <ul>
+      <li><a href="lekplats/transforms-and-transitions/">Lek nu!</a></li>
+      <li><a href="source.php?path=transforms-and-transitions/index.php">Visa källkoden</a></li>
+      <li><a href="http://jsfiddle.net/rcus/y0t4goes/">JSFiddle</a></li>
+    </ul>
+  </h2>
+  <p>Med hjälp av CSS3 går det att forma och flytta element på olika sätt. Här visas lite olika förslag.</p>
+
+  <h2 id="css-sprite-w-css3-transitions">Baddie som springer runt
+    <ul>
+      <li><a href="lekplats/css-sprite-w-css3-transitions/">Lek nu!</a></li>
+      <li><a href="source.php?path=css-sprite-w-css3-transitions/index.php">Visa källkoden</a></li>
+      <li><a href="http://jsfiddle.net/rcus/spvnwz5s/">JSFiddle</a></li>
+    </ul>
+  </h2>
+  <p>En liten figur som springer omkring med hjälp av knapparna på tangentbordet. Skriptet lyssnar efter vissa knapptryckningar. Är det rätt knapp kommer css'en uppdateras och därmed figuren flytta på sig. Figuren kommer kommer från en CSS-sprite som jag har "lånat" från mos.</p>
+  <img src="/img/sprite_mickey_mos.png" alt="">
+
+  <h2 id="resize-element">Ändra elementets storlek
+    <ul>
+      <li><a href="lekplats/resize-element/">Lek nu!</a></li>
+      <li><a href="source.php?path=resize-element/index.php">Visa källkoden</a></li>
+      <li><a href="http://jsfiddle.net/rcus/08d7dmLy/">JSFiddle</a></li>
+    </ul>
+  </h2>
+  <p>Här justeras ett elements storlek till de värden som har angetts i formuläret. Elementets värden hämtas med <code>offsetWidth</code>/<code>offsetHeight</code> och skrivs in i fälten. När formuläret skickas sätts värdena till elementets <code>style</code>-attribut.</p>
+  <p>Det finns en "bugg" i skriptet. Eller ja, den fungerar korrekt, men inte som en användaren kanske förväntar sig. När sidan laddas fylls värdena i (940px/194px). Trycker man på knappen, kommer rutans storlek att ändras. Alltså har den hämtat "fel" värde... Eller? Njae, <code>offsetWidth</code>/<code>offsetHeight</code> motsvarar värdet på elementet (906px/160px), plus 2 &times; padding (16px) och 2 &times; border (1px). Men när man ändrar blir de nya värdena 940px/194px för elementet, plus padding och border som är som tidigare.</p>
+
   <h2 id="mall">Mall
-  <ul>
-    <li><a href="lekplats/mall/">Lek nu!</a></li>
-    <li><a href="source.php?path=mall/index.php">Visa källkoden</a></li>
-    <li><a href="http://jsfiddle.net/rcus/60mj43hv/">JSFiddle</a></li>
-  </ul></h2>
+    <ul>
+      <li><a href="lekplats/mall/">Lek nu!</a></li>
+      <li><a href="source.php?path=mall/index.php">Visa källkoden</a></li>
+      <li><a href="http://jsfiddle.net/rcus/60mj43hv/">JSFiddle</a></li>
+    </ul>
+  </h2>
   <p>Första skriptet. Mest gjort för att skapa en struktur för fortsatt lek ;)</p>
+  <p>Även skapat mappen <a href="lekplats/sample/">sample</a> för att snabbt komma igång med ny programmering.</p>
 
 </div>
 <div class="sidebar">
--- webroot/report.php
+++ webroot/report.php
@@ -16,7 +16,28 @@ $herbert['main'] = <<<EOD
 <div class="boxed">
   <p><em>Här kommer redovisningarna från de olika kursmomenten att presenteras.</em></p>
   <h2 id="kmom01">Kmom01: Kom igång med JavaScript</h2>
-  <p>...</p>
+  <p><strong>Vilken utvecklingsmiljö använder du?</strong><br />
+    Jag har lagt upp en virtuell Ubuntu-server som jag kan ansluta genom hemma-nätverket. Användar blandat Win och Mac, beroende på humör ;) Kodar i Sublime Text och surfar (och därmed testar koden) med Chrome.
+  </p>
+  <p><strong>Hur väl känner du till JavaScript?</strong><br />
+    Tja, jag har använt JavaScript (och JQuery) lite, men inte lärt mig det ordentligt. Löst problemen efter behov, helt enkelt...
+  </p>
+  <p><strong>Vilken uppfattning har du av JavaScript så här långt?</strong><br />
+    Förr tänkte jag att det var inte att ha... Nu, kanon!! Att kunna hantera webbsidor dynamiskt på klient-nivå har sina fördelar. Därmed har jag nog ökat förväntningar på vad JavaScript och kursen.
+  </p>
+  <p><strong>Berätta vilka exempelprogram du gjorde och länka till dem.</strong><br />
+    Jag gjorde de exempelprogram som fanns i övningarna och forsökte att sätta mig in i dem för att förstå hur de hängde ihop. I detta kursmoment känns det som jag fick koll på läget. Kul att göra något som är användarbaserat som att styra runt en baddie.<br />
+    Kom igång snabbt med JSFiddle också, inte använt det eller liknande tidigare. Hade lite problem med att få in LESS-koden för baddien som springer runt, så jag klippte in den kompilerade CSS'en istället. Men vid nästa övning, transforms och transitions, insåg jag att det saknades LESS-kod från base.less. Lurigt med många steg...<br />
+    <a href="playground.php">Lekplatsen</a> | <a href="http://jsfiddle.net/user/rcus/fiddles/">JSFiddle</a>
+  </p>
+  <p><strong>Beskriv hur du gjort din baddie och vilka konster den kan.</strong><br />
+    Nja, min baddie blev en klonad Mickey Mos. Jag prioriterade bort att skapa en egen sprite, så... Och konsterna blev baserade på exemplet.
+  </p>
+  <p><strong>Gjorde du extrauppgiften och utbildade din baddie med något extra?</strong><br />
+    Japp! Men jag det var klurigt att få till två rörelser, så jag höll på att ge upp. Men till slut, genom att dela upp rotationen i två steg, så funkade det. Förflyttningen (transition) riktning baserades på rotationens vinkeln. Så om jag börjar med 90°, kan jag sedan förflytta den i sidled och fortsätta med att rotera ytterligare 630°. Svettigt.<br />
+    Men sen blev det inte mer av extra features.
+  </p>
+
 </div>
 <div class="sidebar">
   <h3>Välj kursmoment:</h3>
