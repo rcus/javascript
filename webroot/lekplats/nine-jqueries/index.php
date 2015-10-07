29e19a05a4c9c9aa146cae9c5121dc476cd4d788
Marcus Törnroth <m@rcus.se>
2015-10-07 22:18:13 +0200

    animation no 5 done

--- webroot/lekplats/nine-jqueries/index.php
+++ webroot/lekplats/nine-jqueries/index.php
@@ -7,13 +7,12 @@
 <div class='giftwrapper'>
 
   <div class='gift boxed'>
-    <!-- Common giftbox-content -->
     <div class='giftbox'>1</div>
     <div class='minimize hidden' title='Stäng'>&times;</div>
 
     <div id='box1' class='thegift hidden'>
       <img src='https://unsplash.it/300/200/?random=1' alt='' />
-      <h1>Första boxen</h1>
+      <h1>Ändra bakgrundsfärg på ett element</h1>
       <p>
         Nu kommer den första boxen. Klicka på ett element, så ska du se att bakgrundsfärgen ändras. Cool!
       </p>
@@ -21,18 +20,70 @@
   </div>
 
   <div class='gift boxed'>
-    <!-- Common giftbox-content -->
     <div class='giftbox'>2</div>
     <div class='minimize hidden' title='Stäng'>&times;</div>
 
     <div id='box2' class='thegift hidden'>
       <img src='https://unsplash.it/300/200/?random=2' alt='' />
-      <h1>Andra boxen</h1>
+      <h1>Förhindra klick-effekt</h1>
       <p>
         I första boxen klickade vi på ett element för att något skulle ske. Denna gång kan du klicka på hela boxen för att ändra bakgrundsfärg. Eller klicka på bilden för att ändra storleken på den.
       </p>
       <p>
         Problemet är att bilden är i boxen, vilket betyder att när du klicka på bilden så aktiveras även boxens funktion. Men det går att stoppa detta. Eller som det heter, stoppa propageringen. Cool!
+      </p>
+    </div>
+  </div>
+
+  <div class='gift boxed'>
+    <div class='giftbox'>3</div>
+    <div class='minimize hidden' title='Stäng'>&times;</div>
+
+    <div id='box3' class='thegift hidden'>
+      <img src='https://unsplash.it/300/200/?random=3' alt='' />
+      <h1>Lägg till och ta bort element</h1>
+      <p>
+        Vi har ändrat på element, men nu ska vi lägga till och ta bort. Lägg till en färgruta genom att ange en färgkod. Klicka på den för att ta bort den. Cool!
+      </p>
+      <p id='palette'>
+        <label for='color'>Färgkod (hex):</label>
+        <input type='text' name='color' id='color' placeholder='#ccc eller #91a040' />
+        <input type='button' id='colorbutton' value='Lägg till färg' />
+      </p>
+    </div>
+  </div>
+
+  <div class='gift boxed'>
+    <div class='giftbox'>4</div>
+    <div class='minimize hidden' title='Stäng'>&times;</div>
+
+    <div id='box4' class='thegift hidden'>
+      <img src='https://unsplash.it/300/200/?random=4' alt='' />
+      <h1>Ändra storlek</h1>
+      <p>
+        Bredd: <span id='width'></span> <input type='button' id='addwidth' value='+'> <input type='button' id='subwidth' value='-'><br />
+        Höjd: <span id='height'></span> <input type='button' id='addheight' value='+'> <input type='button' id='subheight' value='-'>
+      </p>
+      <p>
+        Nu så, nu kör vi en storleksändring på bilden. Visst är den fin? Tja, jag har ingen aning eftersom det är slumpad bild. Bildens storlek är angiven. Tryck på + och - så ändrar du på bredden eller höjden. Cool!
+      </p>
+    </div>
+  </div>
+
+  <div class='gift boxed'>
+    <div class='giftbox'>5</div>
+    <div class='minimize hidden' title='Stäng'>&times;</div>
+
+    <div id='box5' class='thegift hidden'>
+      <img id='fadingimage' src='https://unsplash.it/150/200/?random=5a' alt='' />
+      <img id='slidingimage' src='https://unsplash.it/150/200/?random=5b' alt='' />
+      <h1>Animeringar</h1>
+      <p>
+        Nu ska vi låta bilderna komma till liv. Eller ja, nästan. Vi ska animera dem. Med hjälp av toggleSliding och toggleFading kommer bilderna döljas och visas på ett lite mer levande sätt. Cool!
+      </p>
+      <p class='center'>
+        <input type='button' id='slide' value='Slide. Baby, slide.'>
+        <input type='button' id='fade' value='Fade in. Fade out.'>
       </p>
     </div>
   </div>
--- webroot/lekplats/nine-jqueries/main.js
+++ webroot/lekplats/nine-jqueries/main.js
@@ -11,16 +11,10 @@ $(document).ready(function(){
 
   // Gift-box
   $('.gift div:first-child').click(function() {
-    console.log("clicked on gift");
     toggleGift(this);
-    // $(this).parent().toggleClass('boxed unboxed');
-    // $(this).parent().children('div').toggleClass('hidden');
   });
-
   $('.minimize').click(function() {
     toggleGift(this);
-    // $(this).parent().toggleClass('boxed unboxed');
-    // $(this).parent().children('div').toggleClass('hidden');
   });
 
 
@@ -41,6 +35,58 @@ $(document).ready(function(){
   });
 
 
+  // Box 3 - Add an element and add handler to remove the element on click
+  $('#box3 input[type="button"]').click(function() {
+    $('<div></div>')
+      .insertAfter('#palette')
+      .css({
+        float: 'left',
+        margin: '5px',
+        height: '68px',
+        width: '69px',
+        backgroundColor: $('#box3 input[type="text"]').val(),
+        cursor: 'pointer'
+      })
+      .click(function() {
+        $(this).remove();
+      });
+  });
+
+
+  // Box 4
+  var currentSize = function() {
+    $('#box4 #width').text($('#box4 img').width() + ' px');
+    $('#box4 #height').text($('#box4 img').height() + ' px');
+  };
+  $('#box4').parent().click(function() {
+    $('#box4 img').width($('#box4 img').width());
+    $('#box4 img').height($('#box4 img').height());
+    currentSize();
+  });
+  $('#box4 input[type="button"]').click(function() {
+    currentSize();
+  });
+  $('#box4 #addwidth').click(function() {
+    $('#box4 img').width($('#box4 img').width() + 10);
+  });
+  $('#box4 #subwidth').click(function() {
+    $('#box4 img').width($('#box4 img').width() - 10);
+  });
+  $('#box4 #addheight').click(function() {
+    $('#box4 img').height($('#box4 img').height() + 10);
+  });
+  $('#box4 #subheight').click(function() {
+    $('#box4 img').height($('#box4 img').height() - 10);
+  });
+
+
+  // Box 5
+  $('#box5 #fade').click(function() {
+    $('#box5 #fadingimage').fadeToggle();
+  });
+  $('#box5 #slide').click(function() {
+    $('#box5 #slidingimage').slideToggle();
+  });
 
 
   console.log("Dags för att öppna paket!");
