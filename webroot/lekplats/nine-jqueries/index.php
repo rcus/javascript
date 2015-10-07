19c925892d8ffee9dfefbf96ab62820bbb266f11
Marcus Törnroth <m@rcus.se>
2015-10-07 09:45:17 +0200

    new jquery playground

---
+++ webroot/lekplats/nine-jqueries/index.php
@@ -0,0 +1,7 @@
+<?php $title='Nio små jQuery-rätter'; include(__DIR__ . '/../mall/header.php'); ?>
+ 
+<div id='flash'>
+  <p id='text' class='red'>Denna text ska ersättas när sidan och DOM är laddade.</p>
+</div>
+ 
+<?php $path=__DIR__; include(__DIR__ . '/../mall/footer.php'); ?>
---
+++ webroot/lekplats/nine-jqueries/main.js
@@ -0,0 +1,12 @@
+/**
+ * Place your JS-code here.
+ */
+$(document).ready(function(){
+  'use strict';
+  var text;
+  text = document.getElementById('text');
+  text.innerHTML = 'Hello World!<br />Nu är dokumentet färdigladdat.';
+  text.className = 'green';
+  console.log('Allt är färdigt.');  
+  
+});
