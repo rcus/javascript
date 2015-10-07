27eb0beaf57c33d779bd364903ff8313aa129292
Marcus Törnroth <m@rcus.se>
2015-10-07 14:15:37 +0200

    two gifts done

--- webroot/lekplats/nine-jqueries/index.php
+++ webroot/lekplats/nine-jqueries/index.php
@@ -1,13 +1,42 @@
 <?php $title='Nio små jQuery-rätter'; include(__DIR__ . '/../mall/header.php'); ?>
 
-<div id='flash'>
-  <p id='text' class='red'>Denna text ska ersättas när sidan och DOM är laddade.</p>
-</div>
+<h1>
+  Nio små jQuery-rätter
+</h1>
 
-<div id='box1' class='box'>
-  <img src='https://unsplash.it/200/300/?random' alt='' />
-  <h1>Första boxen</h1>
-  <p>Nu kommer den första boxen. Klicka på ett element, så ska du se att bakgrundsfärgen ändras. Cool!</p>
+<div class='giftwrapper'>
+
+  <div class='gift boxed'>
+    <!-- Common giftbox-content -->
+    <div class='giftbox'>1</div>
+    <div class='minimize hidden' title='Stäng'>&times;</div>
+
+    <div id='box1' class='thegift hidden'>
+      <img src='https://unsplash.it/300/200/?random=1' alt='' />
+      <h1>Första boxen</h1>
+      <p>
+        Nu kommer den första boxen. Klicka på ett element, så ska du se att bakgrundsfärgen ändras. Cool!
+      </p>
+    </div>
+  </div>
+
+  <div class='gift boxed'>
+    <!-- Common giftbox-content -->
+    <div class='giftbox'>2</div>
+    <div class='minimize hidden' title='Stäng'>&times;</div>
+
+    <div id='box2' class='thegift hidden'>
+      <img src='https://unsplash.it/300/200/?random=2' alt='' />
+      <h1>Andra boxen</h1>
+      <p>
+        I första boxen klickade vi på ett element för att något skulle ske. Denna gång kan du klicka på hela boxen för att ändra bakgrundsfärg. Eller klicka på bilden för att ändra storleken på den.
+      </p>
+      <p>
+        Problemet är att bilden är i boxen, vilket betyder att när du klicka på bilden så aktiveras även boxens funktion. Men det går att stoppa detta. Eller som det heter, stoppa propageringen. Cool!
+      </p>
+    </div>
+  </div>
+
 </div>
 
 <?php $path=__DIR__; include(__DIR__ . '/../mall/footer.php'); ?>
--- webroot/lekplats/nine-jqueries/main.js
+++ webroot/lekplats/nine-jqueries/main.js
@@ -4,10 +4,43 @@
 $(document).ready(function(){
   'use strict';
 
+  var toggleGift = function(elem) {
+    $(elem).parent().toggleClass('boxed unboxed');
+    $(elem).parent().children('div').toggleClass('hidden');
+  };
+
+  // Gift-box
+  $('.gift div:first-child').click(function() {
+    console.log("clicked on gift");
+    toggleGift(this);
+    // $(this).parent().toggleClass('boxed unboxed');
+    // $(this).parent().children('div').toggleClass('hidden');
+  });
+
+  $('.minimize').click(function() {
+    toggleGift(this);
+    // $(this).parent().toggleClass('boxed unboxed');
+    // $(this).parent().children('div').toggleClass('hidden');
+  });
+
+
+  // Box 1
   $('#box1 h1, #box1 p, #box1 img').click(function() {
     $(this).toggleClass('dark');
-    console.log("toggle!");
   });
+
+
+  // Box 2
+  $('#box2 img').click(function( event ) {
+    event.stopPropagation();
+    $(this).toggleClass('thumbnail');
+  });
+
+  $('#box2').click(function() {
+    $(this).parent().toggleClass('yellowed');
+  });
+
+
 
 
   console.log("Dags för att öppna paket!");
