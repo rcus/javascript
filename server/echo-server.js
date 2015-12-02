2dca9375934da8fcc480b7bd1dc428456e3d0c38
Marcus Törnroth <m@rcus.se>
2015-12-02 08:59:56 +0100

    echo-server added

---
+++ server/echo-server.js
@@ -0,0 +1,16 @@
+var port = 8179;
+ 
+// Require the modules we need
+var http = require('http');
+ 
+// Create a http server with a callback handling all requests
+var httpServer = http.createServer(function(request, response) {
+  console.log((new Date()) + ' Received request for ' + request.url);
+  response.writeHead(200, {'Content-type': 'text/plain'});
+  response.end('Hello world\n');
+});
+ 
+// Setup the http-server to listen to a port
+httpServer.listen(port, function() {
+  console.log((new Date()) + ' HTTP server is listening on port ' + port);
+});
